3d0981d275e055a5954d7b73380bb3646cf4aed0
NeroSnowblade <fajarmustaqim11@gmail.com>
2020-09-12 21:00:51 +0700

    Layout Forgot Password
    Pull Template dari Alvan, nggk beda jauh si hasil akhirnya :v . Tinggal rada di Back-End

---
+++ app/src/main/java/com/application/a4_school/ForgotPassword.java
@@ -0,0 +1,14 @@
+package com.application.a4_school;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
