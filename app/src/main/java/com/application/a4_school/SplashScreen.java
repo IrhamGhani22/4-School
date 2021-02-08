55883aa8665f9a89757cfbf0326b896e313527c3
MFadlulHafiizh <muh.hafiizh56@gmail.com>
2021-02-08 09:57:54 +0700

    tambahan animasi splashscreen

--- app/src/main/java/com/application/a4_school/SplashScreen.java
+++ app/src/main/java/com/application/a4_school/SplashScreen.java
@@ -5,16 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 import com.application.a4_school.Auth.Login;
 
 public class SplashScreen extends AppCompatActivity {
     private int delayscreen = 1500;
+    private TextView txt_splash;
 
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        txt_splash = findViewById(R.id.splashschool);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                txt_splash.setVisibility(View.VISIBLE);
+                txt_splash.animate().alpha(1.0f).setDuration(200);
+            }
+        },500);
 
         new Handler().postDelayed(new  Runnable(){
             @Override
