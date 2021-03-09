e7f1c4ffca6bc8b29456d9a94db4d1d0c1c9b6f0
M Fadlul Hafiizh <muh.hafiizh56@gmail.com>
2021-03-09 11:19:56 +0700

    fix schedule fragment

--- app/src/main/java/com/application/a4_school/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/com/application/a4_school/ui/schedule/ScheduleFragment.java
@@ -1,28 +1,23 @@
 package com.application.a4_school.ui.schedule;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.application.a4_school.Models.Schedule;
 import com.application.a4_school.Models.ScheduleData;
 import com.application.a4_school.R;
 import com.application.a4_school.adapter.GridScheduleAdapter;
-import com.application.a4_school.ui.help.HelpViewModel;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ScheduleFragment extends Fragment {
     RecyclerView rv_Schedule;
