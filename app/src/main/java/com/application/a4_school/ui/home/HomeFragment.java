29e89e41de82450b2e1b5dcb35215e44dcf3fae6
MFadlulHafiizh <muh.hafiizh56@gmail.com>
2021-03-24 11:30:19 +0700

    clean checkpoint & realtime greeting good morning/afternoon/evening etc

--- app/src/main/java/com/application/a4_school/ui/home/HomeFragment.java
+++ app/src/main/java/com/application/a4_school/ui/home/HomeFragment.java
@@ -32,24 +32,38 @@ import com.application.a4_school.ui.schedule.ScheduleFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import static com.application.a4_school.R.id.WebView1;
 import static com.application.a4_school.R.id.content;
 import static com.application.a4_school.R.id.rv_mapel;
+import static com.application.a4_school.R.id.time;
 
 public class HomeFragment extends Fragment {
     private HomeViewModel HomeViewModel;
     RecyclerView rv;
     HomeListAdapter adapter;
-    TextView shName;
-    private ArrayList<Home> homeList = new ArrayList<>() ;
+    TextView shName, shGreeting;
+    private ArrayList<Home> homeList = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-     View root = inflater.inflate(R.layout.fragment_home, container, false);
-
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
         shName = root.findViewById(R.id.nameHome);
+        shGreeting = root.findViewById(R.id.greeting_text);
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+        if(timeOfDay >= 0 && timeOfDay < 12){
+            shGreeting.setText("Good Morning,");
+        }else if(timeOfDay >= 12 && timeOfDay < 16){
+            shGreeting.setText("Good Afternoon,");
+        }else if(timeOfDay >= 16 && timeOfDay < 21){
+            shGreeting.setText("Good Evening,");
+        }else if(timeOfDay >= 21 && timeOfDay < 24){
+            shGreeting.setText("Good Night,");
+        }
+
         String name = getActivity().getSharedPreferences("userInfo", 0).getString("name", "Hmm something wen't wrong i cant see your name):");
         shName.setText(name);
         rv = root.findViewById(rv_mapel);
@@ -60,7 +74,7 @@ public class HomeFragment extends Fragment {
 
     }
 
-    public void showList(){
+    public void showList() {
         homeList.addAll(HomeData.getlisthome());
         rv.setLayoutManager(new LinearLayoutManager(getActivity()));
         adapter = new HomeListAdapter(homeList, getActivity());
@@ -71,7 +85,7 @@ public class HomeFragment extends Fragment {
             public void onItemClicked(Home homeList) {
                 BottomNavigationView navigationView = getActivity().findViewById(R.id.nav_view);
                 Fragment fragment = null;
-                switch (homeList.getJudul()){
+                switch (homeList.getJudul()) {
                     case "JOBS":
                         fragment = new ScheduleFragment();
                         navigationView.setSelectedItemId(R.id.nav_jobs);
